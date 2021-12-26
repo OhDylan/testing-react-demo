a09e7c2aebf5092f4ab9e93ed9ed455920fdd079
Oh Dylan <ddragon1128@hotmail.com>
2021-12-26 22:52:47 +0800

    Added pokemon component and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import SubscribeForm from './components/SubscribeForm';
+import SubscribeForm from './components/SubscribeForm/SubscribeForm';
+import PokeSearch from './components/PokeSearch/PokeSearch';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <SubscribeForm />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SubscribeForm />} />
+          <Route path="/pokemon" element={<PokeSearch />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/components/PokeSearch/PokeSearch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PokeSearch = () => {
+    return (<div>Hello World</div>)
+}
+
+export default PokeSearch;
